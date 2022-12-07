fa4fc76e311739331771855946b578c6e4aa65e4
russellpp <russellroypalmejar@gmail.com>
2022-12-07 20:23:17 +0800

    Finish account container, cont on itemlist

--- my-app/src/components/expcomponents/AccountContainer.jsx
+++ my-app/src/components/expcomponents/AccountContainer.jsx
@@ -1,45 +1,68 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import imgAdd from '../assets/add-icon.svg';
+import imgAdd from "../assets/add-icon.svg";
 import BudgetModal from "./BudgetModal";
 import WalletList from "./WalletList";
 
 function AccountContainer(props) {
-    const [isBudgetOpen, setIsBudgetOpen] = useState(false);
-    const [walletList, setWalletList] = useState([])
-    const [currentWallet, setCurrentWallet] = useState(null)
-    const {currentUser, setCurrentUser, accounts, setAccounts} = props;
+  const [isBudgetOpen, setIsBudgetOpen] = useState(false);
+  const [walletList, setWalletList] = useState([]);
+  const {
+    currentUser,
+    setCurrentUser,
+    accounts,
+    setAccounts,
+    currentWallet,
+    setCurrentWallet,
+  } = props;
+  
+  // finding current wallet
+  
+  const listWallets = currentUser?.wallets || [];
 
-    const listWallets = currentUser?.wallets || []
 
-    // finding current wallet
-
-    useEffect(() => {
-        if (listWallets.length > 0){
-            const selectedWallet = listWallets.find((wallet) => wallet.isCurrentAccount);
-            setCurrentWallet(selectedWallet);
-        }
-    }, [walletList, accounts])
-
-    
-    function OpenBudgetModal(){
-        setIsBudgetOpen(true);
-        
+  useEffect(() => {
+    if (listWallets.length > 0) {
+      const selectedWallet = listWallets.find(
+        (wallet) => wallet.isCurrentAccount
+      );
+      setCurrentWallet(selectedWallet);
     }
+  }, [walletList, accounts]);
 
+  function OpenBudgetModal() {
+    setIsBudgetOpen(true);
+  }
 
-    return(
-        <div className="AccountContainer">
-            <button className="AddAccountButton" onClick={OpenBudgetModal}>
-                    <img src={imgAdd}/>
-                 Account
-            </button>
-            {isBudgetOpen && <BudgetModal walletList={walletList} setWalletList={setWalletList} setIsBudgetOpen={setIsBudgetOpen} currentUser={currentUser} accounts={accounts} setAccounts={setAccounts} currentWallet={currentWallet} setCurrentWallet={setCurrentWallet}/>}
-            <WalletList currentUser={currentUser} listWallets={listWallets} currentWallet={currentWallet} setCurrentWallet={setCurrentWallet} accounts={accounts} setAccounts={setAccounts} />
-            
-        </div>
-    )
+  return (
+    <div className="AccountContainer">
+      <button className="AddAccountButton" onClick={OpenBudgetModal}>
+        <img src={imgAdd} />
+        Account
+      </button>
+      {isBudgetOpen && (
+        <BudgetModal
+          walletList={walletList}
+          setWalletList={setWalletList}
+          setIsBudgetOpen={setIsBudgetOpen}
+          currentUser={currentUser}
+          accounts={accounts}
+          setAccounts={setAccounts}
+          currentWallet={currentWallet}
+          setCurrentWallet={setCurrentWallet}
+        />
+      )}
+      <WalletList
+        currentUser={currentUser}
+        listWallets={listWallets}
+        currentWallet={currentWallet}
+        setCurrentWallet={setCurrentWallet}
+        accounts={accounts}
+        setAccounts={setAccounts}
+      />
+    </div>
+  )
 }
 
-export default AccountContainer
+export default AccountContainer;
--- my-app/src/components/expcomponents/BudgetModal.jsx
+++ my-app/src/components/expcomponents/BudgetModal.jsx
@@ -1,90 +1,103 @@
 import React from "react";
 import { useState } from "react";
 
-function BudgetModal({walletList, setWalletList, setIsBudgetOpen, currentUser, accounts, setAccounts}) {
-    
+function BudgetModal({
+  walletList,
+  setWalletList,
+  setIsBudgetOpen,
+  currentUser,
+  accounts,
+  setAccounts,
+  currentWallet,
+  setCurrentWallet
+}) {
+  let [budgetValue, setBudgetValue] = useState();
+  let [budgetAccount, setBudgetAccount] = useState();
 
+  function CloseBudgetModal() {
+    setIsBudgetOpen(false);
+  }
 
-    let [budgetValue, setBudgetValue] = useState();
-    let [budgetAccount, setBudgetAccount] = useState();
-    
+  function getValue(budget) {
+    setBudgetValue(budget.target.value);
+    budgetValue = budget.target.value;
+  }
 
-    function CloseBudgetModal(){
-        setIsBudgetOpen(false)
+  function getAccount(budgetAcct) {
+    setBudgetAccount(budgetAcct.target.value);
+    budgetAccount = budgetAcct.target.value;
+  }
+
+  function createNewBudget() {
+    const wallets = currentUser.wallets;
+    if (wallets.length > 0) {
+      wallets.map((wallet) => {
+        wallet.isCurrentAccount = false;
+      });
     }
+    const newWallet = {
+      name: budgetAccount,
+      balance: budgetValue,
+      isCurrentAccount: true,
+      records: [],
+    };
+    wallets.push(newWallet);
+    const updatedAccount = {
+      ...currentUser,
+      wallets: wallets,
+    };
+    const updatedAccounts = accounts.map((account) => {
+      if (account.email === currentUser.email) {
+        return updatedAccount;
+      } else {
+        return account;
+      }
+    });
+    const selectedWallet = wallets.find((wallet) => wallet.isCurrentAccount)
+    console.log(selectedWallet);
+    setCurrentWallet(selectedWallet);
+    setAccounts(updatedAccounts);
+    CloseBudgetModal();
+  }
 
-    function getValue(budget){
-        setBudgetValue(budget.target.value);
-        budgetValue = budget.target.value;
-    }
-
-    function getAccount(budgetAcct){
-        setBudgetAccount(budgetAcct.target.value);
-        budgetAccount = budgetAcct.target.value;
-    }
-
-    
-
-    function createNewBudget () {
-        const wallets = currentUser.wallets
-        if (wallets.length > 0){
-            wallets.map((wallet) => {
-                wallet.isCurrentAccount = false
-            })
-        }
-        const newWallet = {
-            name: budgetAccount,
-            balance: budgetValue,
-            isCurrentAccount: true,
-            records: []
-            } 
-        wallets.push(newWallet)
-        const updatedAccount = {
-            ...currentUser,
-            wallets: wallets
-        
-            }
-        const updatedAccounts = accounts.map((account)=>{
-            if(account.email === currentUser.email){
-                return updatedAccount 
-            }
-            else{
-                return account
-            }
-            
-        })
-        setWalletList(wallets)
-        setAccounts(updatedAccounts)
-        CloseBudgetModal()
-        
-    }
-
-    return(
-        <div className = "BudgetModal">
-            <div className="BudgetModalContent">
-                <div className="BudgetModalHeader">
-                    <h4 className='BudgetModalTitle'>New Account</h4>
-                </div>
-                <div className="BudgetModalBody">
-                    <label htmlFor="accountName">Budget account name: </label>
-                    <input type="text" name='amountName' id='amountName' autoComplete='off' autoFocus onChange={getAccount}/>
-            
-                   <label htmlFor="amount">Enter amount: </label>
-                   <input type="number" name='amount' id='amount' autoComplete='off' autoFocus onChange={getValue}/>
-                </div>
-                <div className="BudgetModalFooter">
-                    <button className="NewBudgetAccountButton" onClick={createNewBudget}>Add Account</button>
-                    <button className="BudgetCancelButton" onClick={CloseBudgetModal}>Cancel</button>
-                </div>
-            </div>  
+  return (
+    <div className="BudgetModal">
+      <div className="BudgetModalContent">
+        <div className="BudgetModalHeader">
+          <h4 className="BudgetModalTitle">New Account</h4>
         </div>
-    )
-    
+        <div className="BudgetModalBody">
+          <label htmlFor="accountName">Budget account name: </label>
+          <input
+            type="text"
+            name="amountName"
+            id="amountName"
+            autoComplete="off"
+            autoFocus
+            onChange={getAccount}
+          />
 
-
+          <label htmlFor="amount">Enter amount: </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            autoComplete="off"
+            autoFocus
+            onChange={getValue}
+          />
+        </div>
+        <div className="BudgetModalFooter">
+          <button className="NewBudgetAccountButton" onClick={createNewBudget}>
+            Add Account
+          </button>
+          <button className="BudgetCancelButton" onClick={CloseBudgetModal}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default BudgetModal
-
-
-
+export default BudgetModal;
